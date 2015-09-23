5af74480e3e437f0c340e244257e4c8618aee0ce
popescu2 <popescu2@pdx.edu>
2015-09-22 23:04:43 -0700

    Added swipe views to the study cards section.

---
+++ app/src/main/java/opensourceproject/kanjisteps/StudyFragmentB.java
@@ -0,0 +1,38 @@
+package opensourceproject.kanjisteps;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Bob on 9/21/2015.
+ */
+public class StudyFragmentB extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_b, container, false);
+        populateCard(savedInstanceState, view);
+
+        return view;
+    }
+
+    public void populateCard(Bundle args, View view)
+    {
+
+        TextView txtKanji = (TextView)view.findViewById(R.id.txtKanji);
+        TextView txtMeaning = (TextView)view.findViewById(R.id.txtEngMeaning);
+        TextView txtOnyomi = (TextView)view.findViewById(R.id.txtOnyomi);
+        TextView txtKunyomi = (TextView)view.findViewById(R.id.txtKunyomi);
+
+        txtKanji.setText("Kanji: " + getArguments().getString("kanji"));
+        txtMeaning.setText("English Definition: " + getArguments().getString("meaning"));
+        txtOnyomi.setText("Onyomi Reading: " + getArguments().getString("onyomi"));
+        txtKunyomi.setText("Kunyomi Reading: " + getArguments().getString("kunyomi"));
+
+    }
+
+}
--- app/src/main/java/opensourceproject/kanjisteps/Study_Slides.java
+++ app/src/main/java/opensourceproject/kanjisteps/Study_Slides.java
@@ -7,13 +7,21 @@ Detailed Licensing information can be found in the COPYING file
 package opensourceproject.kanjisteps;
 
 import android.app.ActionBar;
+import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.drawable.GradientDrawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -22,27 +30,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import static android.view.GestureDetector.*;
 
 
-public class Study_Slides extends ActionBarActivity implements
-            OnGestureListener,OnDoubleTapListener{
+public class Study_Slides extends FragmentActivity {
+
+    ViewPager viewPager = null;
+    RelativeLayout relativeLayout = null;
 
     public String level_marker;
     KanjiToStudyAdapter db;
     Cursor cursor;
-    LinearLayout layout;
+    int currentCursorPosition;
 
-    private GestureDetectorCompat gestureDetect;
+
+    //private GestureDetectorCompat gestureDetect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study__slides);
-        gestureDetect = new GestureDetectorCompat(this, this);
-        gestureDetect.setOnDoubleTapListener(this);
-        layout = (LinearLayout) View.inflate(this, R.layout.activity_study__slides, null);
+        //gestureDetect = new GestureDetectorCompat(this, this);
+        //gestureDetect.setOnDoubleTapListener(this);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null)
@@ -54,8 +65,86 @@ public class Study_Slides extends ActionBarActivity implements
         cursor = db.getItemsByLevel(level_marker);
         cursor.moveToNext();
 
-        StudyByLevel(cursor, db);
+        currentCursorPosition = cursor.getPosition();
+
+        viewPager = (ViewPager)findViewById(R.id.pager);
+        viewPager.setOffscreenPageLimit(1);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        viewPager.setAdapter(new CardAdapter(fragmentManager));
+
+        //StudyByLevel(cursor, db);
     }
+
+    class CardAdapter extends FragmentStatePagerAdapter {
+
+        public CardAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            Log.d("Matt", "get count called");
+            int temp = cursor.getCount();
+            return temp;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Log.d("Matt", "get item is called " + i);
+
+            cursor.moveToPosition(i);
+
+            int count = cursor.getCount();
+
+            Fragment fragment = null;
+
+            Bundle args = new Bundle();
+
+            if(i==cursor.getPosition()) {
+                //cursor.moveToPrevious();
+                setBundleArgs(cursor, args);
+                if (i == count -1)
+                    fragment = new StudyFragmentB();
+                else fragment = new StudyFragmentA();
+                fragment.setArguments(args);
+                //cursor.moveToNext();
+
+            }
+            /*
+            else if(i==currentCursorPosition+1)
+            {
+                setBundleArgs(cursor, args);
+                fragment = new StudyFragmentA();
+                fragment.setArguments(args);
+                cursor.moveToNext();
+            }
+            else if(i==currentCursorPosition+2)
+            {
+                //cursor.moveToNext();
+                setBundleArgs(cursor, args);
+                fragment = new StudyFragmentA();
+                fragment.setArguments(args);
+                //cursor.moveToPrevious();
+                cursor.moveToNext();
+            }
+            */
+            return fragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            int currentCursorLocation = cursor.getPosition();
+            int kanjiColumn = cursor.getColumnIndex(db.myKanjiDb.COLUMN_KANJI);
+            Log.d("Matt", "get title called on " + position);
+            cursor.moveToPosition(position);
+            String title = cursor.getString(kanjiColumn);
+            cursor.moveToPosition(currentCursorLocation);
+
+            return title;
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -103,82 +192,6 @@ public class Study_Slides extends ActionBarActivity implements
 
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        gestureDetect.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        //TextView txt = (TextView)findViewById(R.id.txtKanji);
-        Button btn = (Button) findViewById(R.id.btnLastItem);
-
-        if(velocityX < -2500) {
-            if(cursor.moveToNext())
-            {
-                //cursor.moveToNext();
-                StudyByLevel(cursor, db);
-                btn.setVisibility(View.INVISIBLE);
-            }
-            else
-            {
-                cursor.moveToPrevious();
-                btn.setVisibility(View.VISIBLE);
-
-            }
-        }
-        else if(velocityX > 2500) {
-            if(cursor.moveToPrevious())
-            {
-                StudyByLevel(cursor, db);
-                btn.setVisibility(View.INVISIBLE);
-            }
-            else cursor.moveToNext();
-        }
-        return false;
-    }
-
     public void goToQuizByLevel(View view)
     {
         KanjiToStudyAdapter myKanjiDb = new KanjiToStudyAdapter(this);
@@ -191,5 +204,19 @@ public class Study_Slides extends ActionBarActivity implements
     public void lastItemPressed(View view)
     {
         goToQuizByLevel(view);
+    }
+
+    public void setBundleArgs(Cursor cursor, Bundle args)
+    {
+        int kanjiColumn = cursor.getColumnIndex(db.myKanjiDb.COLUMN_KANJI);
+        int meaningColumn = cursor.getColumnIndex(db.myKanjiDb.COLUMN_MEANING);
+        int onyomiColumn = cursor.getColumnIndex(db.myKanjiDb.COLUMN_ON_READING);
+        int kunyomiColumn = cursor.getColumnIndex(db.myKanjiDb.COLUMN_KUN_READING);
+
+        args.putString("kanji", cursor.getString(kanjiColumn));
+        args.putString("meaning", cursor.getString(meaningColumn));
+        args.putString("onyomi", cursor.getString(onyomiColumn));
+        args.putString("kunyomi", cursor.getString(kunyomiColumn));
+
     }
 }
